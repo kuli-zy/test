40c0dd0c30bab530d3603ecc788c12111a146589
kuli-zy <zy88999988@gmail.com>
2026-09-12 16:25:48 +0800

    test: add Spring Boot context test

---
+++ src/test/java/com/example/snake/SnakeGameApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.snake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
