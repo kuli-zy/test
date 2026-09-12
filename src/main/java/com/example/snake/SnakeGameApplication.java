8361b0d3d4ce4dc33fe298677bcd316eb5d2b11d
kuli-zy <zy88999988@gmail.com>
2026-09-12 16:25:00 +0800

    feat: add Spring Boot application entry point

---
+++ src/main/java/com/example/snake/SnakeGameApplication.java
@@ -0,0 +1,12 @@
+package com.example.snake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnakeGameApplication.class, args);
+    }
+}
